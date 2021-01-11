67ca01932f974572312aedf3bd678cc8c21774d4
ann-sys <62380821+ann-sys@users.noreply.github.com>
2021-01-11 17:08:11 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const path = require('path')
+const authRoutes = require('./routes/auth')
+const subsoiluserRoutes = require('./routes/subsoilUser')
+const fieldRoutes = require('./routes/field')
+const wellRoutes = require('./routes/well')
+const extractionRoutes = require('./routes/extraction')
+const keys = require('./config/keys')
+const app = express()
+
+mongoose
+    .connect(keys.mongoURI)
+    .then(() => console.log('MongoDB connected.'))
+    .catch((error) => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(require('cors')())
+app.use('/api/auth', authRoutes)
+app.use('/api/subsoiluser', subsoiluserRoutes)
+app.use('/api/field', fieldRoutes)
+app.use('/api/well', wellRoutes)
+app.use('/api/extraction', extractionRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist/client'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+        )
+    })
+}
+
+module.exports = app
